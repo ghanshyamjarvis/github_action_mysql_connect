b2a9e5ecc1540e45d795f73f9ebfe62be1fd19f9
ghanshyamjarvis <57344531+ghanshyamjarvis@users.noreply.github.com>
2020-07-29 20:37:39 +0530

    3005

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const express =require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 const server = app.listen(port, function() {
   console.log('API listening on port ' + port);
