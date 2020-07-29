5927190aa474f88d0f3023f122d35fadd26c10bb
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-29 15:35:36 +0530

    database connect mysql

---
+++ app.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const express =require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, function() {
+  console.log('API listening on port ' + port);
+});
+
+
+const connection = mysql.createConnection({
+  host:'127.0.0.1',
+  user:'root',
+  password:'password',
+  database:'company'
+});
+connection.connect()
+console.log('database connected')
