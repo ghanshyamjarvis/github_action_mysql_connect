45b1467452e9e466870886939f5adc04fb53df3c
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-30 11:21:12 +0530

    final done

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const server = app.listen(port, function() {
   console.log('API listening on port ' + port);
 });
 
-
 const connection = mysql.createConnection({
   host:'localhost',
   user:'root',
   password:'password',
   port: 8888
 });
-connection.connect()
-console.log('database connected')
+ connection.connect()
+ console.log('database connected')
 connection.end()
 console.log('database connected11')
 server.close();
